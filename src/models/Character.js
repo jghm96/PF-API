9cd4610bc54d9c8bcfd01346dd3f1023c8e1563c
jghm96 <77647023+jghm96@users.noreply.github.com>
2022-01-31 18:10:25 -0300

    comentario

--- src/models/Character.js
+++ src/models/Character.js
@@ -3,6 +3,7 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
+  // comentario
   sequelize.define('character', {
     name: {
       type: DataTypes.STRING,
