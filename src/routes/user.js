fbdd44118e45d55ad0bdc385e8b36ece537b1dd1
Angel <angelhdez416@gmail.com>
2022-02-02 16:59:27 -0600

    Delete User comments

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,23 +4,6 @@ const { User, Pair, Susbcription } = require('../db')
 const isAuthenticated = require('../Authenticated')
 const transporter = require('../Mail')
 
-user.get('/email', isAuthenticated, async (req, res) => {
-  console.log(req.user.email)
-  try{
-    await transporter.sendMail({
-      from: '"Crypto" <cryptohenryp@gmail.com>',
-      to: req.user.email, //email del usuario que esta loggeado
-      subject:"Mensaje de prueba", // Subject del correo
-      //text: "Prueba de correo",
-      html: `<h1>Hola</h1>`, //Codigo hmtl que quieres que se muestre en el correo
-    })
-    res.json({message: 'Email send'})
-  }catch(e){
-    console.log(e)
-    return res.status(400).json({message: 'Something goes wrong'})
-  }
-})
-
 
 user.get('/usuario', isAuthenticated, (req, res) => {
   res.json(req.user)
@@ -43,27 +26,7 @@ user.get('/', isAuthenticated ,async (req, res) => {
   }
 })
 
-user.post('/create', isAuthenticated,async (req, res) => { //Ruta para crear en la BD registros para trabajar sin la api
-  try{
-    let {password, email, balance, userType, mobile} = req.body
-    const {username, id} = req.user
 
-    await Pair.create({
-      id: 0,
-      price: 1000,
-      pair: 'asdasd'
-    })
-    await Pair.create({
-      id: 1,
-      price: 1000,
-      pair: 'asdfds'
-    })  
-
-    return res.json(username)
-  }catch(err){
-    res.status(500).json(err)
-  }
-})
 
 user.post('/subscribe', isAuthenticated ,async (req, res) => { //Ruta para subscripcion a cryptos que existan en la BD
   let {pair, priceRise, priceFall } = req.body
