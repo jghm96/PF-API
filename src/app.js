96c99aaa537879d0b7c47313d00e9b8ec6842483
Angel <angelhdez416@gmail.com>
2022-02-04 08:52:03 -0600

    Url cambios

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
@@ -19,7 +20,7 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin',['https://pfapi2.herokuapp.com','http://localhost:3000']); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', process.env.CLIENT_URL); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
