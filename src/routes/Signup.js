29ce8066f29d172b20934dbb47ec9fc50b22a0e2
Angel <angelhdez416@gmail.com>
2022-02-04 19:57:29 -0600

    Verificacion de cuenta existente

--- src/routes/Signup.js
+++ src/routes/Signup.js
@@ -10,13 +10,23 @@ signUp.post("/", async (req, res) => {
     const {username, password,email} = req.body;
     try{
         let cryptPassword = await bcrypt.hash(password,10);
-        await User.create({id,username,password: cryptPassword,email});
-        res.status(201).json({email, username});
+        const find = await User.findOne({
+            where:{
+                [Op.or]:[{username},{email}]
+            }
+        })
+        if(find)
+          res.status(490).send({error:"username o email existentes"});
+        else{
+          await User.create({id,username,password: cryptPassword,email});
+          res.status(201).json({succes:"user create"});
+        }
     }catch(e){
        console.log(e);
        res.status(401).json(e);
     }
 });
+
 
 module.exports = signUp;
 
