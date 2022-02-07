e9dc1f8f31e94e6cf7bb351bd96292674cdd6764
Angel <angelhdez416@gmail.com>
2022-02-07 08:04:33 -0600

    Login route

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -12,7 +12,7 @@ login.post('/',passport.authenticate('local'), (req, res) => {
 login.get("/google", passport.authenticate("google",{scope:["profile","email"]}));
 
 login.get("/googlecallback", passport.authenticate("google",{
-    successRedirect:'http://localhost:3001/login/success',
+    successRedirect:'http://localhost:3001/home/',
     failureRedirect:'http://localhost:3001/login/error',
 
 }));
