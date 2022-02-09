b4557625c793119b8cbae2938feae199c71929a8
aramayomartin <raul.aramayo@ib.edu.ar>
2022-02-09 11:51:44 -0300

    Comments

--- src/routes/Transaction.js
+++ src/routes/Transaction.js
@@ -3,7 +3,7 @@ const {Transaction,Symbol,User} = require("../db");
 const transactions = Router();
 const isAuthenticated = require("../Authenticated");
 
-transactions.post("/buy",isAuthenticated,async(req,res) => {
+transactions.post("/buy",isAuthenticated,async(req,res) => { // Permite crear una nueva transaccion
 
     const {symbol,deposit,whitdraw,userId} = req.params;
 
@@ -18,7 +18,7 @@ transactions.post("/buy",isAuthenticated,async(req,res) => {
     res.json({message:"Transaction completed successfully."});
 });
 
-transactions.get("/state-account",isAuthenticated,async(req,res) => {
+transactions.get("/state-account",isAuthenticated,async(req,res) => { // Permite ver el estado de la cuenta, saldos, movmientos y rendimientos.
     const {userId} = req.params;
     const results = Transaction.findAll({
         where:{ userId},
@@ -56,7 +56,10 @@ transactions.get("/state-account",isAuthenticated,async(req,res) => {
         }
     });
     h.currentBalance = currentBalance;
-    res.json(h);
+    res.json(h); // Esto es un objeto con tres elementos:
+                // All transactions podria servir para hacer un movimientos historicos de la cuenta
+                // Historical by Symbol podria servir para mostrar el rendimiento en un simbolo en particular.
+                // Current Balance da cuenta del estado actual de la cuenta bajo todos los simbolos disponibles en la wallet.
 });
 
 module.exports = transactions;
