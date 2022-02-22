b39269586550769365891c46b22e22bc16b56bee
Angel <angelhdez416@gmail.com>
2022-02-22 16:18:27 -0600

    Settings fixed

--- src/routes/Settings.js
+++ src/routes/Settings.js
@@ -8,7 +8,6 @@ settings.post('/', isAuthenticated, async (req, res) => {
     let { image, theme, passwordChange, lastPassword, newPassword } = req.body
     const user = await User.findByPk(req.user.id)
     const match = await bcrypt.compare(lastPassword, user.toJSON().password)
-    console.log(match)
     if(passwordChange && !match) return res.status(404).json({errorType: 'errorUser', errorCode: '1150', errorMessage:'Password do not match with the registry'});
     const newPassword2 = passwordChange ? await bcrypt.hash(newPassword, 10) : null
     await user.update({
